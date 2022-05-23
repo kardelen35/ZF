fde00c1b8db816ff13c790d0da66393aad60187b
kardelen35 <kardelen081simsek@gmail.com>
2022-05-23 14:47:33 +0300

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,41 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ButtonModule} from 'primeng/button';
-import {AccordionModule} from 'primeng/accordion';     
-import {MenuItem} from 'primeng/api';  
-import {InputTextModule} from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { AccordionModule } from 'primeng/accordion';
+import { MenuItem } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
 
-import {MatFormFieldModule} from '@angular/material/form-field';
-
-import {ToastModule} from 'primeng/toast';
-import {CalendarModule} from 'primeng/calendar';
-import {SliderModule} from 'primeng/slider';
-import {MultiSelectModule} from 'primeng/multiselect';
-import {ContextMenuModule} from 'primeng/contextmenu';
-import {DialogModule} from 'primeng/dialog';
-import {DropdownModule} from 'primeng/dropdown';
-import {ProgressBarModule} from 'primeng/progressbar';
+import { ToastModule } from 'primeng/toast';
+import { CalendarModule } from 'primeng/calendar';
+import { SliderModule } from 'primeng/slider';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressBarModule } from 'primeng/progressbar';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MetricTypePipe } from './Pipe/metric-type.pipe';
-import { ReactiveFormsModule} from '@angular/forms';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PercentPipe } from '@angular/common';
-import {AutoCompleteModule} from 'primeng/autocomplete';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 import { WireVersionPipe } from '../app/Pipe/wire-version.pipe';
-import {TableModule} from 'primeng/table';
-
-
+import { TableModule } from 'primeng/table';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MetricTypePipe,
-    WireVersionPipe,
-  ],
+  declarations: [AppComponent, MetricTypePipe, WireVersionPipe],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -45,28 +35,22 @@ import {TableModule} from 'primeng/table';
     ButtonModule,
     AccordionModule,
     CalendarModule,
-		SliderModule,
-		DialogModule,
+    SliderModule,
+    DialogModule,
     HttpClientModule,
-		MultiSelectModule,
-		ContextMenuModule,
-		DropdownModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    DropdownModule,
     FormsModule,
-		ButtonModule,
-		ToastModule,
+    ButtonModule,
+    ToastModule,
     InputTextModule,
     ProgressBarModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatAutocompleteModule,
-    MatInputModule,
     BrowserAnimationsModule,
-    AutoCompleteModule
-    
-    
-    
+    AutoCompleteModule,
   ],
-  providers: [MetricTypePipe,WireVersionPipe],
-  bootstrap: [AppComponent]
+  providers: [MetricTypePipe, WireVersionPipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
